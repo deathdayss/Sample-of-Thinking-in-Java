4c55d8134aab106e13c51db9e758b411ca0d7a2f
deathdayss <zhichengwangss@gmail.com>
2019-12-07 12:12:05 +0800

    Initial commit

---
+++ src/Commentsanddocumentation/Documentation1.java
@@ -0,0 +1,10 @@
+package Commentsanddocumentation;
+
+//: Comments_and_Documentation/Documentation1.java
+/** A class comment */
+public class Documentation1 {
+    /** A field comment */
+    public int i;
+    /** A method comment */
+    public void f() {}
+} ///:~

6d2cf7361cb270bbaa51fa683a28cfefe41b2d07
deathdayss <zhichengwangss@gmail.com>
2020-01-04 18:47:13 +0800

    The correct answer of Ex 8

--- src/accesscontrol/ConnectionManager.java
+++ src/accesscontrol/ConnectionManager.java
@@ -1,17 +1,30 @@
 package accesscontrol;
 
 class Connection {
-
+    private Connection() {}
+    static Connection makeConnection() {
+        return new Connection();
+    }
 }
 
 public class ConnectionManager {
-    private static int left = 0;
+    private static int left = 10;
     private static Connection[] k = new Connection[10];
+    static {
+        System.out.println("initialization block");
+        for (int i = 0; i < k.length; i++) {
+            k[i] = Connection.makeConnection();
+        }
+    }
     static Connection getConnection() {
-        if (left < 10) {
-            left++;
-            return k[left];
+        if (left > 0) {
+            return k[--left];
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        Connection a = ConnectionManager.getConnection();
+        System.out.println(a);
     }
 }

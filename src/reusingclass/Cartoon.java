f7fa2bd4666609c4c07a1da712a233e452b5bc4a
deathdayss <zhichengwangss@gmail.com>
2020-01-05 22:31:53 +0800

    Initializing the base class

---
+++ src/reusingclass/Cartoon.java
@@ -0,0 +1,25 @@
+package reusingclass;
+
+//: reusing/Cartoon.java
+// Constructor calls during inheritance.
+
+class Art {
+    int k = 0;
+    {
+        System.out.println("I am the art instance initialization");
+    }
+    Art() { System.out.println("Art constructor"); }
+}
+class Drawing extends Art {
+    Drawing() { System.out.println("Drawing constructor"); }
+}
+public class Cartoon extends Drawing {
+    public Cartoon() { System.out.println("Cartoon constructor"); }
+    public static void main(String[] args) {
+        Cartoon x = new Cartoon();
+    }
+} /* Output:
+Art constructor
+Drawing constructor
+Cartoon constructor
+*///:~

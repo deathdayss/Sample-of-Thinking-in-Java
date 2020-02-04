af130812962b563850487d8bf874b4a38556685a
Zhicheng Wang <zhichengwangS@outlook.com>
2020-02-04 22:23:06 +1100

    Map

---
+++ src/holdingyourobject/PetMap.java
@@ -0,0 +1,24 @@
+package holdingyourobject;
+
+//import typeinfo.pets.*;
+//import java.util.*;
+//import static net.mindview.util.Print.*;
+//public class PetMap {
+//    public static void main(String[] args) {
+//        Map<String,Pet> petMap = new HashMap<String,Pet>();
+//        petMap.put("My Cat", new Cat("Molly"));
+//        petMap.put("My Dog", new Dog("Ginger"));
+//        petMap.put("My Hamster", new Hamster("Bosco"));
+//        System.out.println(petMap);
+//        Pet dog = petMap.get("My Dog");
+//        System.out.println(dog);
+//        System.out.println(petMap.containsKey("My Dog"));
+//        System.out.println(petMap.containsValue(dog));
+//    }
+//}
+/* Output:
+{My Cat=Cat Molly, My Hamster=Hamster Bosco, My Dog=Dog Ginger}
+Dog Ginger
+true
+true
+*///:~

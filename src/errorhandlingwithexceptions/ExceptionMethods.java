a418e38389ff6615cc051dbd11ec523ee2ea8c3c
Zhicheng Wang <zhichengwangS@outlook.com>
2020-02-09 14:27:45 +1100

    Catching any exception

---
+++ src/errorhandlingwithexceptions/ExceptionMethods.java
@@ -0,0 +1,25 @@
+package errorhandlingwithexceptions;
+
+public class ExceptionMethods {
+    public static void main(String[] args) {
+        try {
+            throw new Exception("My Exception");
+        } catch(Exception e) {
+            System.out.println("Caught Exception");
+            System.out.println("getMessage():" + e.getMessage());
+            System.out.println("getLocalizedMessage():" +
+                    e.getLocalizedMessage());
+            System.out.println("toString():" + e);
+            System.out.println("printStackTrace():");
+            e.printStackTrace(System.out);
+        }
+    }
+} /* Output:
+Caught Exception
+getMessage():My Exception
+getLocalizedMessage():My Exception
+toString():java.lang.Exception: My Exception
+printStackTrace():
+java.lang.Exception: My Exception
+at ExceptionMethods.main(ExceptionMethods.java:8)
+*///:~
